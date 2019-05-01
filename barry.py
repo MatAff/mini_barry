7b27bf003e254664859732458292756140260a5d
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2019-04-30 22:16:08 -0600

    Adding color filter

--- barry.py
+++ barry.py
@@ -31,12 +31,13 @@ class display(object):
 class filter(object):
 	
 	def __init__(self, lower, upper):
-		self.lower = np.array(lower)
-		self.upper = np.array(upper)
+		self.bounds = {'lower': np.array(lower), 'upper': np.array(upper)}
+		self.change = ('lower',0)
+		self.delta = 5
 
 	def apply(self, bgr_frame, erode=False, dilate=False):    
 		hsv_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv_frame, self.lower, self.upper)
+		mask = cv2.inRange(hsv_frame, self.bounds['lower'], self.bounds['upper'])
 		
 		if erode == True:
 			kernel = np.ones((5,5),np.uint8)
@@ -49,8 +50,41 @@ class filter(object):
 		res = cv2.bitwise_and(bgr_frame, bgr_frame, mask=mask)
 		
 		return(res, mask)		
+		
+	def key_handler(self, key):		
+		if key == H_KEY: 
+			print('Pressed h')
+			if self.change[0] == 'lower':
+				self.change = ('upper', 0)
+			else:
+				self.change = ('lower', 0)
+		if key == S_KEY: 
+			print('Pressed s')
+			if self.change[0] == 'lower':
+				self.change = ('upper', 1)
+			else:
+				self.change = ('lower', 1)
+		if key == V_KEY: 
+			print('Pressed v')
+			if self.change[0] == 'lower':
+				self.change = ('upper', 2)
+			else:
+				self.change = ('lower', 2)		
+		elif key == PLUS_KEY:
+			print('Pressed +') 
+			self.bounds[self.change[0]][self.change[1]] = self.bounds[self.change[0]][self.change[1]] + self.delta 
+			print(self.bounds)
+		elif key == MIN_KEY:
+			print('Pressed -') 
+			self.bounds[self.change[0]][self.change[1]] = self.bounds[self.change[0]][self.change[1]] - self.delta 
+			print(self.bounds)
 
-ESC_KEY = 27
+ESC_KEY = 27 
+H_KEY = 104
+S_KEY = 115
+V_KEY = 118
+PLUS_KEY = 43 
+MIN_KEY = 45
 
 running = True
 key = ESC_KEY
@@ -58,15 +92,28 @@ key = ESC_KEY
 cam = camera()
 disp = display()
 
-yellow_filter = filter([5,0,0], [75,255,255])
+color_filter = filter([20,50,20], [70,255,255])
 
 while running:
+	
+	# Get frame
 	rval, frame = cam.get()
+	
 	if rval == True:
-		frame, mask = yellow_filter.apply(frame, False, True)
+		
+		# Apply filter
+		frame, mask = color_filter.apply(frame, True, False)
+		
+		# Display
 		key = disp.show(frame)
+		
+		# Handle keys
+		if key != -1: print(key)
+		color_filter.key_handler(key)
+	
+	# Check status
 	if key == ESC_KEY:
-		break
+		running = False
 
 cam.release()
 disp.close()	
