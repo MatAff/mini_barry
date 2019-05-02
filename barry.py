c47d986f34087bae325479c86dda6a8177e275a3
MatAff <mathijsaffourtit@gmail.com>
2019-05-02 21:37:40 +0100

    Add line follow functionality and start debug

--- barry.py
+++ barry.py
@@ -5,14 +5,14 @@ import numpy as np
 
 from visual import *
 from color_filter import *
-from drive import * # Not available on laptop
+from drive import * # Not available on laptop 
 
 ESC_KEY = 27
 
-KEY_FORWARD = 0
-KEY_BACKWARD = 0
-KEY_LEFT = 0
-KEY_RIGHT = 0
+KEY_FORWARD = 82
+KEY_BACKWARD = 84
+KEY_LEFT = 81
+KEY_RIGHT = 83
 
 running = True
 key = ESC_KEY
@@ -23,39 +23,47 @@ twist = Twist()
 ada_drive = AdaDrive() # Not available on laptop
 
 #green_filter = Filter([20,0,20], [70,255,255])
-green_filter = Filter([30, 135, 0], [55, 260, 55]) # Daytime 
+green_filter = Filter([30, 50, 0], [55, 260, 255]) # Daytime 
 
 while running:
-	
-	# Get frame
-	rval, frame = cam.get()
-	
-	if rval == True:
-		
-		# Apply filter
-		frame, mask = green_filter.apply(frame, True, False)
-		
-		# Get position
-		avg_pos = green_filter.get_pos(mask, 240, 50)
-		
-		# Display
-		key = disp.show(frame)
-		
-		# Handle keys
-		if key != -1: print(key)
-		green_filter.key_handler(key)
-		
-		# Manual drive
-		if key == [KEY_FORWARD, KEY_BACKWARD, KEY_LEFT, KEY_RIGHT] :
-			twist.man(key)
-				
-		# Drive
-		ada_drive.drive(twist) # Not available on laptop
-			
-	# Check status
-	if key == ESC_KEY:
-		running = False
+    
+    # Get frame
+    rval, frame = cam.get()
+    
+    if rval == True:
+        
+        # Apply filter
+        frame, mask = green_filter.apply(frame, True, False)
+        
+        # Get position
+        avg_pos = green_filter.get_pos(mask, 300, 50)
+        print(avg_pos)
 
+        # Set drive
+        rotate = (avg_pos - 320.0) / -320.0 * 0.1
+        print(rotate)
+        twist.set_rotate(rotate)
+        
+        # Display
+        key = disp.show(frame)
+        
+        # Handle keys
+        if key != -1: print(key)
+        green_filter.key_handler(key)
+        
+        # Manual drive
+        if key in [KEY_FORWARD, KEY_BACKWARD, KEY_LEFT, KEY_RIGHT] :
+            twist.man(key)
+            twist.print()
+                
+        # Drive
+        ada_drive.drive(twist) # Not available on laptop
+            
+    # Check status
+    if key == ESC_KEY:
+        running = False
+
+ada_drive.stop()
 cam.release()
-disp.close()	
+disp.close()    
 
