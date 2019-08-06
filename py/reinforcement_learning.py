53bc3f62d139576bb71913b3ec23fa02be5d5ac9
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2019-08-06 17:35:15 -0600

    slowly switch on rl

--- py/reinforcement_learning.py
+++ py/reinforcement_learning.py
@@ -122,21 +122,21 @@ class RLStateAction(RLBase):
             #self.act_mimic = self.model_mimic.predict(np.array([state]))[0,0]
             self.act_mimic = self.model_mimic_ols.predict(np.array([state]))[0]
             self.before_after = np.append(self.before_after, np.array([[action, self.act_mimic]]), axis=0)
-#        if self.run_nr > 0:
-#            pos_actions = np.arange(-0.25, 0.25, 0.05)
-#            val = np.empty((0,1))
-#            for act in pos_actions:
-#                X = np.append(state, act)
-#                y = self.model_state_action.predict(np.array([X]))[0]
-#                val = np.append(val, y)
-#            if val.size > 0:
-#                print(val)
-#                best_act = pos_actions[np.argmax(val)]
-#                print('rl action: %.2f' % best_act)
-#                self.act_state_action = best_act
         if self.run_nr > 0:
-#            p = np.min([(self.run_nr - 1.0) / 10.0, 1.0])
-#            self.act = (1 - p) * self.act_mimic + p * self.act_state_action
+            pos_actions = np.arange(-0.25, 0.25, 0.05)
+            val = np.empty((0,1))
+            for act in pos_actions:
+                X = np.append(state, act)
+                y = self.model_state_action.predict(np.array([X]))[0]
+                val = np.append(val, y)
+            if val.size > 0:
+                print(val)
+                best_act = pos_actions[np.argmax(val)]
+                print('rl action: %.2f' % best_act)
+                self.act_state_action = best_act
+        if self.run_nr > 0:
+            p = np.min([(self.run_nr - 1.0) / 20.0, 1.0])
+            self.act = (1 - p) * self.act_mimic + p * self.act_state_action
             self.act = self.act_mimic
 
         print(np.round(action, 3), " >> ", np.round(self.act, 3))
